f3acb4d1f1cf7b3e677714475008fd8ffd940047
jpena <juan.pena@wmcontractor.com>
2025-02-17 16:02:04 -0500

    Builder pattern exercise solved

--- 01-creacionales/01-builder.ts
+++ 01-creacionales/01-builder.ts
@@ -1,3 +1,4 @@
+import { COLORS } from "../helpers/colors.ts";
 /**
  * ! Patrón Builder:
  * Es un patrón de diseño creacional que nos permite construir objetos complejos
@@ -12,3 +13,71 @@
  *
  * https://refactoring.guru/es/design-patterns/builder
  */
+
+class Computer {
+  public cpu: string = "cpu - not defined";
+  public ram: string = "ram - not defined";
+  public storage: string = "storage - not defined";
+  public gpu: string = "gpu - not defined";
+
+  displayConfiguration() {
+    console.log(`Computer config
+            CPU: ${this.cpu}
+            RAM: ${this.ram}
+            storage: ${this.storage}
+            GPU: ${this.gpu}
+        `);
+  }
+}
+
+class ComputerBuilder {
+  private computer: Computer;
+  constructor() {
+    this.computer = new Computer();
+  }
+
+  setCPU(cpu: string): ComputerBuilder {
+    this.computer.cpu = cpu;
+    return this;
+  }
+
+  setRAM(ram: string): ComputerBuilder {
+    this.computer.ram = ram;
+    return this;
+  }
+
+  setStorage(storage: string): ComputerBuilder {
+    this.computer.storage = storage;
+    return this;
+  }
+
+  setGPU(gpu: string): ComputerBuilder {
+    this.computer.gpu = gpu;
+    return this;
+  }
+
+  build() {
+    return this.computer;
+  }
+}
+
+function main() {
+  const basicComputer: Computer = new ComputerBuilder()
+    .setCPU("Intel Core 2")
+    .setRAM("4GB")
+    .setStorage("256GB")
+    .build();
+
+  const gamerComputer: Computer = new ComputerBuilder()
+    .setCPU("Intel core i9")
+    .setRAM("32GB")
+    .setStorage("2TB SSD")
+    .setGPU("Nvidia RTX 4070")
+    .build();
+
+  console.log("%cBasic Computer: ", COLORS.cyan);
+  basicComputer.displayConfiguration();
+  gamerComputer.displayConfiguration();
+}
+
+main();
--- 01-creacionales/01.2-builder.ts
+++ 01-creacionales/01.2-builder.ts
@@ -11,7 +11,7 @@
  * * que lo componen.
  */
 
-import { COLORS } from '../helpers/colors.ts';
+import { COLORS } from "../helpers/colors.ts";
 
 //! Tarea: crear un QueryBuilder para construir consultas SQL
 /**
@@ -50,37 +50,55 @@ class QueryBuilder {
   }
 
   select(...fields: string[]): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.fields = fields;
+    return this;
   }
 
   where(condition: string): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.conditions.push(condition);
+    return this;
   }
 
-  orderBy(field: string, direction: 'ASC' | 'DESC' = 'ASC'): QueryBuilder {
-    throw new Error('Method not implemented.');
+  orderBy(field: string, direction: "ASC" | "DESC" = "ASC"): QueryBuilder {
+    this.orderFields.push(`${field} ${direction}`);
+    return this;
   }
 
   limit(count: number): QueryBuilder {
-    throw new Error('Method not implemented.');
+    this.limitCount = count;
+    return this;
   }
 
   execute(): string {
     // Select id, name, email from users where age > 18 and country = 'Cri' order by name ASC limit 10;
-    throw new Error('Method not implemented.');
+    const fields = this.fields.length > 0 ? this.fields.join(", ") : "*";
+    const conditions =
+      this.conditions.length > 0
+        ? `WHERE ${this.conditions.join(" AND ")}`
+        : "";
+    const orderBys =
+      this.orderFields.length > 0
+        ? `ORDER BY ${this.orderFields.join(", ")}`
+        : "";
+
+
+    const limit = this.limitCount ? `LIMIT ${this.limitCount}` : "";
+
+    return `SELECT ${fields} FROM ${this.table} ${conditions} ${orderBys} ${limit}`;
   }
 }
 
 function main() {
-  const usersQuery = new QueryBuilder('users')
-    .select('id', 'name', 'email')
-    .where('age > 18')
+  const usersQuery = new QueryBuilder("users")
+    .select("id", "name", "email")
+    .where("age > 18")
     .where("country = 'Cri'") // Esto debe de hacer una condición AND
-    .orderBy('name', 'ASC')
+    .orderBy("name", "ASC")
+    .orderBy("email", "DESC")
     .limit(10)
     .execute();
 
-  console.log('%cConsulta:\n', COLORS.red);
+  console.log("%cQuery:\n", COLORS.orange);
   console.log(usersQuery);
 }
 

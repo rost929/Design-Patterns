91bd33d34c345fe6ea9aff39cdcddc9b58bdffba
jpena <juan.pena@wmcontractor.com>
2025-03-17 14:34:35 -0500

    Factory function exercises solved

--- 01-creacionales/07-factory-function.ts
+++ 01-creacionales/07-factory-function.ts
@@ -1,3 +1,5 @@
+import { COLORS } from "../helpers/colors.ts";
+
 /**
  * ! Factory Function
  * Es un patrón de diseño que nos permite crear objetos o funciones de manera dinámica que serán
@@ -7,3 +9,28 @@
  * * es decir, en tiempo de ejecución y no en tiempo de compilación.
  *
  */
+type Language = "es" | "en" | "fr";
+
+function createGreeter(lang: Language) {
+  return function (name: string) {
+    const messages = {
+      es: `Hola , %c${name}!`,
+      en: `Hello, %c${name}!`,
+      fr: `Salut, %c${name}!`,
+    };
+
+    return console.log(messages[lang], COLORS.red);
+  };
+}
+
+function main() {
+    const spanishGreeter = createGreeter("es");
+    const englishGreeter = createGreeter("en");
+    const frenchGreeter = createGreeter("fr");
+
+    spanishGreeter("Fernando");
+    englishGreeter("Alice");
+    frenchGreeter("Amelie");
+}
+
+main();
